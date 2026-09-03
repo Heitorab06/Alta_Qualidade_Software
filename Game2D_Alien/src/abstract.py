fa6f19d301468b3d8570dcc5379b058afcd02b37
Heitor Barreto <habarreto06@gmail.com>
2026-09-03 10:38:36 -0300

    feat: aplicando principio liskov

--- Game2D_Alien/src/abstract.py
+++ Game2D_Alien/src/abstract.py
@@ -7,26 +7,39 @@ class Desconto(ABC):
     def calcular(self, valor):
         pass
 
+class IDesconto:
+    def calcular(self, valor):
+        raise NotImplementedError
 
-class DescontoNormal(Desconto):
-    def __init__(self):
-        pass
 
+class ICupom:
+    def aplicar_cupom(self, codigo):
+        raise NotImplementedError
+
+
+class IVIP:
+    def calcular(self, usuario):
+        raise NotImplementedError
+
+
+class DescontoNormal(IDesconto):
     def calcular(self, valor):
         return valor*0.1
 
 
-class DescontoVip(Desconto):
-    def __init__(self):
-        pass
-
+class DescontoVip(IDesconto, ICupom, IVIP):
     def calcular(self, valor):
         return valor*0.2
 
+    def aplicar_cupom(self, codigo):
+        return True
 
-class DescontoPremium(Desconto):
-    def __init__(self):
-        pass
+    def validar_usuario_vip(self, usuario):
+        return usuario == "vip"
 
+
+class DescontoPremium(IDesconto):
     def calcular(self, valor):
         return valor*0.3
+
+
--- Game2D_Alien/src/main.py
+++ Game2D_Alien/src/main.py
@@ -1,20 +1,27 @@
 from abstract import *
 
-tipo = (str(input("Qual o tipo do desconto?"))).lower()
-valor = int(input("Qual o valor?"))
 
-premium = DescontoPremium()
+def aplicar_desconto(desconto: Desconto, valor:float) -> float:
+    return desconto.calcular(valor)
+
+def aplicar_cupom(desconto: Desconto, cupom: str) -> bool:
+    return desconto.aplicar_cupom(cupom)
+
+tipo = (str(input("Qual o tipo do desconto? "))).lower()
+valor = float(input("Qual o valor? "))
+
 vip = DescontoVip()
 normal = DescontoNormal()
 
 
 match tipo:
     case "normal":
-        print(normal.calcular(valor))
+        print("Desconto: ", aplicar_desconto(normal, valor))
     case "vip":
-        print(vip.calcular(valor))
-    case "premium":
-        print(premium.calcular(valor))
+        print("Desconto: ", aplicar_desconto(vip, valor))
+        print("Cupom VIP: ", aplicar_cupom(vip, "DESC10"))
     case _:
         print("Tipo inválido")
+
+
 
